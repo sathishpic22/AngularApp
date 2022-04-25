ce96fde68129cf2648a6c4c2add73d358132903a
sathishpic22 <58845085+sathishpic22@users.noreply.github.com>
2022-04-25 11:52:31 +0530

    First Angular Commit

---
+++ AngularSpringbootTest/src/main/java/com/example/demo/AngularSpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootTestApplication.class, args);
+	}
+
+}
